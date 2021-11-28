d2ca9012eb1608718dd992389ff8af7d99c546d3
SeongMin-K <eo1tks3@naver.com>
2021-11-28 15:51:34 +0900

    Initial Commit

---
+++ Singleton_Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton_Tutorial
+//
+//  Created by SeongMinK on 2021/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
